64e4afa8a046379461849c4654180d6476828551
Bobi <slobodanjovanovski@yahoo.com>
2022-01-15 17:52:05 +0800

    Updates

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,43 +1,82 @@
 import Flip from "../Flip";
 import styled from "styled-components";
 
+const MainWrapper = styled.div`
+  margin: 2rem;
+  display: flex;
+  flex-direction: column;
+`
+
 const AppContent = styled.div`
   padding: 2rem;
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  width: auto;
+  max-width: 64rem;
+  margin: 0 auto;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 1rem;
+`
 
-  > div {
-    margin: .5rem;
-  }
+const Title = styled.div`
+  text-align: center;
 `
 
 const App = () => (
-  <AppContent>
-    <Flip
-      flippedContent='I have been flipped'
-      normalContent='Click to flip me'
-    />
-    <Flip
-      flippedContent='I have been flipped'
-      normalContent='Click to flip me'
-    />
-    <Flip
-      flippedContent='I have been flipped'
-      normalContent='Click to flip me'
-    />
-    <Flip
-      flippedContent='I have been flipped'
-      normalContent='Click to flip me'
-    />
-    <Flip
-      flippedContent='I have been flipped'
-      normalContent='Click to flip me'
-    />
-    <Flip
-      flippedContent='I have been flipped'
-      normalContent='Click to flip me'
-    />
-  </AppContent>
+  <MainWrapper>
+    <Title>
+      Учење низ игра
+    </Title>
+    <AppContent>
+      <Flip
+        flippedContent='I have been flipped'
+        normalContent='Click to flip me'
+      />
+      <Flip
+        flippedContent='I have been flipped'
+        normalContent='Click to flip me'
+      />
+      <Flip
+        flippedContent='I have been flipped'
+        normalContent='Click to flip me'
+      />
+      <Flip
+        flippedContent='I have been flipped'
+        normalContent='Click to flip me'
+      />
+      <Flip
+        flippedContent='I have been flipped'
+        normalContent='Click to flip me'
+      />
+      <Flip
+        flippedContent='I have been flipped'
+        normalContent='Click to flip me'
+      />
+      <Flip
+        flippedContent='I have been flipped'
+        normalContent='Click to flip me'
+      />
+      <Flip
+        flippedContent='I have been flipped'
+        normalContent='Click to flip me'
+      />
+      <Flip
+        flippedContent='I have been flipped'
+        normalContent='Click to flip me'
+      />
+      <Flip
+        flippedContent='I have been flipped'
+        normalContent='Click to flip me'
+      />
+      <Flip
+        flippedContent='I have been flipped'
+        normalContent='Click to flip me'
+      />
+      <Flip
+        flippedContent='I have been flipped'
+        normalContent='Click to flip me'
+      />
+    </AppContent>
+  </MainWrapper>
 )
 
 export default App;
